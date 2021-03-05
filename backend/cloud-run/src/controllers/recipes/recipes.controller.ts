51911615354a37a1e00d6a63a57cbfe466cd1b3a
nicollite <nickconte70@gmail.com>
2021-03-05 13:57:41 -0300

    create /recipe and /user controllers

--- backend/cloud-run/src/controllers/recipes/recipes.controller.ts
+++ backend/cloud-run/src/controllers/recipes/recipes.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, Types } from "mongoose";
+import { IRecipe } from "shared";
+import { Recipe, RecipeDocument } from "src/schemas/recipes";
 
-@Controller('recipes')
-export class RecipesController {}
+export interface GetAllRecipesQuery {
+  /** The last id to query */
+  lastId?: string;
+  pageSize: number;
+}
+
+export interface GetRecipeParam {
+  id: string;
+}
+
+@Controller("recipes")
+export class RecipesController {
+  constructor(@InjectModel(Recipe.name) private recipeModel: Model<RecipeDocument>) {}
+
+  @Get()
+  async getAllRecipes(@Query() query: GetAllRecipesQuery) {
+    // Destructure the query string, set the lastId to a initial object
+    const { lastId = 0, pageSize = 10 } = query;
+
+    return this.recipeModel
+      .find({ _id: { $gt: new Types.ObjectId(lastId) } })
+      .sort({ _id: 1 })
+      .limit(pageSize)
+      .exec();
+  }
+
+  @Get("/:id")
+  async getRecipe(@Param() params: GetRecipeParam) {
+    // Destructure the query string, set the lastId to a initial object
+    const { id } = params;
+
+    return this.recipeModel
+      .find({ _id: id })
+      .exec()
+      .then(docs => docs.shift());
+  }
+
+  @Post()
+  async createRecipe(@Body() body: IRecipe | IRecipe[]) {
+    if (Array.isArray(body)) {
+      const recipes = body.map(recipe => new this.recipeModel(recipe));
+      return this.recipeModel.insertMany(recipes).then(docs => docs.map(doc => doc.toJSON()));
+    } else {
+      return new this.recipeModel(body).save().then(doc => doc.toJSON());
+    }
+  }
+
+  @Put()
+  async updateRecipe(@Body() body: IRecipe) {
+    return this.recipeModel.updateOne(
+      { _id: new Types.ObjectId(body._id) },
+      new this.recipeModel(body)
+    );
+  }
+}
--- backend/cloud-run/src/controllers/user/user.controller.ts
+++ backend/cloud-run/src/controllers/user/user.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, Types } from "mongoose";
+import { IUser } from "shared";
+import { User, UserDocument } from "src/schemas/user";
 
-@Controller('user')
-export class UserController {}
+export interface GetUserParam {
+  id: string;
+}
+
+@Controller("user")
+export class UserController {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  @Get("/:id")
+  async getUser(@Param() params: GetUserParam) {
+    // Destructure the query string, set the lastId to a initial object
+    const { id } = params;
+
+    return this.userModel
+      .find({ _id: id })
+      .exec()
+      .then(docs => docs.shift());
+  }
+
+  @Post()
+  async createUser(@Body() body: IUser) {
+    return new this.userModel(body).save().then(doc => doc.toJSON());
+  }
+
+  @Put()
+  async updateUser(@Body() body: IUser) {
+    return this.userModel.updateOne(
+      { $or: [{ _id: new Types.ObjectId(body._id) }, { uid: body.uid }, { email: body.email }] },
+      { $set: { ...body } }
+    );
+  }
+}
