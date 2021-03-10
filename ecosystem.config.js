2f3c59cff2b6e4fae045f183132f63bed7973260
nicollite <nickconte70@gmail.com>
2021-03-10 14:22:35 -0300

    add env config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,17 +5,24 @@ module.exports = {
       script: "yarn start",
       cwd: "./frontend/recipes-fontend",
       combine_logs: true,
-      log_file: "../../logs/frontend.log"
+      log_file: "../../logs/frontend.log",
     },
     {
       name: "backend",
-      script: "yarn start",
+      script: "yarn start:dev",
       cwd: "./backend/cloud-run",
       combine_logs: true,
       log_file: "../../logs/backend.log",
       env: {
-        GOOGLE_APPLICATION_CREDENTIALS: "./credentials/credentials-dev.json"
-      }
-    }
-  ]
+        GOOGLE_APPLICATION_CREDENTIALS: "./credentials/credentials-dev.json",
+      },
+    },
+    {
+      name: "docker-compose up",
+      script: "docker-compose up",
+      cwd: ".",
+      combine_logs: true,
+      log_file: "./logs/mongodb.log",
+    },
+  ],
 };
